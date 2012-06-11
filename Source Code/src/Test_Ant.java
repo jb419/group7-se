befee6d76a8f06328770fa51f340fdb872c811aa
Rajbir Bal <rb271@sussex.ac.uk>
2012-06-11 03:03:20 +0100

    added to Ant_Test

--- Source Code/src/Test_Ant.java
+++ Source Code/src/Test_Ant.java
@@ -97,7 +97,7 @@ public class Test_Ant{
 		o[1] = 51;
 		w.mark(o, AntColour.Black, 0);
 		a.sense(Condition.BlackMarker, SensingDirection.Ahead);
-		assertTrue(a.sense(Condition.BlackMarker, SensingDirection.Ahead) == true);
+		assertTrue(a.sense(Condition.BlackMarker, SensingDirection.Ahead) == 0);
 	}
 	//Tests that checks if a Ant leaves a sense marker.
 	@Test
@@ -120,7 +120,59 @@ public class Test_Ant{
 	//Tests that checks if an Ant is surrounded.
 	@Test
 	public void test_isSurrounded(){
-		Ant a = new Ant(null, null);
-		assertTrue(a.isSurrounded() == true);
+		WorldToken[][] world = new WorldToken[150][150];
+		for (int i = 0; i < 150; i++){
+		for(int c = 0; c < 150; c++){
+		world[i][c] = new WorldToken(WorldTokenType.Empty);
+		}
+		}
+		World w = World.getNewWorld("C:\\Users\\Razor\\Documents\\GitHub\\group7-se\\brain.ant", "C:\\Users\\Razor\\Documents\\GitHub\\group7-se\\brain.ant", world);
+		int[] t = new int[2];
+		t[0] = 50;
+		t[1] = 50;
+		Ant a = new Ant(w, AntColour.Black);
+		w.setAntAt(t, a);
+		int[] q = new int[2];
+		q[0] = 50;
+		q[1] = 51;
+		Ant b = new Ant(w, AntColour.Red);
+		w.setAntAt(t, b);
+		int[] e = new int[2];
+		e[0] = 50;
+		e[1] = 49;
+		Ant c = new Ant(w, AntColour.Red);
+		w.setAntAt(t, c);
+		int[] x = new int[2];
+		x[0] = 51;
+		x[1] = 50;
+		Ant g = new Ant(w, AntColour.Red);
+		w.setAntAt(t, g);
+		int[] r = new int[2];
+		r[0] = 51;
+		r[1] = 50;
+		Ant d = new Ant(w, AntColour.Red);
+		w.setAntAt(t, d);
+		int[] y = new int[2];
+		y[0] = 51;
+		y[1] = 49;
+		Ant f = new Ant(w, AntColour.Red);
+		w.setAntAt(t, f);
+		int[] z = new int[2];
+		z[0] = 49;
+		z[1] = 50;
+		Ant h = new Ant(w, AntColour.Red);
+		w.setAntAt(t, h);
+		int[] k = new int[2];
+		k[0] = 49;
+		k[1] = 50;
+		Ant i = new Ant(w, AntColour.Red);
+		w.setAntAt(t, i);
+		int[] l = new int[2];
+		l[0] = 49;
+		l[1] = 49;
+		Ant j = new Ant(w, AntColour.Red);
+		w.setAntAt(t, j);
+		a.move();
+		assertTrue(a.isAlive() == true);
 	}
 }
