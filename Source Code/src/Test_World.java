aa9fabee092f1501aad7b3aff6f8d6449e1e4fa7
Rajbir Bal <rb271@sussex.ac.uk>
2012-06-11 07:08:50 +0100

    Added test_World

--- Source Code/src/Test_World.java
+++ Source Code/src/Test_World.java
@@ -11,7 +11,7 @@ import org.junit.Test;
  */
 public class Test_World
 {
-
+	//Test to see if the location of the ant is equal to the position that it is inserted into the world.
 	@Test
 	public void test_antAt()
 	{
@@ -28,7 +28,38 @@ public class Test_World
 		Ant a = new Ant(w, AntColour.Black);
 		w.setAntAt(t, a);
 		a.getPos();
-		assertEquals(t, a);
+		a.setPos(t);
+		assertEquals(a, t);
+	}
+	
+	//Test to see if the find location code works.
+	@Test
+	public void test_findAnt()
+	{
+		WorldToken[][] world = new WorldToken[150][150];
+		for (int i = 0; i < 150; i++){
+		for(int c = 0; c < 150; c++){
+		world[i][c] = new WorldToken(WorldTokenType.Empty);
+		}
+		}
+		World w = World.getNewWorld("C:\\Users\\Razor\\Documents\\GitHub\\group7-se\\brain.ant", "C:\\Users\\Razor\\Documents\\GitHub\\group7-se\\brain.ant", world);
+		int[] t = new int[2];
+		t[0] = 50;
+		t[1] = 50;
+		Ant a = new Ant(w, AntColour.Black);
+		w.setAntAt(t, a);
+		assertTrue(findAnt() == true);
+	}
+
+	/**
+	 * The findAnt method
+	 *
+	 * @return
+	 */
+	private boolean findAnt()
+	{
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
