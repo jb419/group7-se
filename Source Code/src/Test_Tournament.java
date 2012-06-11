33cbff4230a5ff57def2140a7145022e1269d686
Rajbir Bal <rb271@sussex.ac.uk>
2012-06-11 04:06:01 +0100

    Finished Tournament

--- Source Code/src/Test_Tournament.java
+++ Source Code/src/Test_Tournament.java
@@ -12,10 +12,17 @@ import org.junit.Test;
 public class Test_Tournament
 {
 
+	//Test to see if the tournament has a world.
 	@Test
-	public void test()
-	{
-		fail("Not yet implemented");
+	public void test_hasWorld()
+	{	
+		WorldToken[][] world = new WorldToken[150][150];
+		for (int i = 0; i < 150; i++){
+		for(int c = 0; c < 150; c++){
+		world[i][c] = new WorldToken(WorldTokenType.Empty);
 	}
-
+	}
+	World w = World.getNewWorld("C:\\Users\\Razor\\Documents\\GitHub\\group7-se\\brain.ant", "C:\\Users\\Razor\\Documents\\GitHub\\group7-se\\brain.ant", world);
+	assert(w.hashCode() != 0);
+	}
 }
